014790b23e5deba75ce34e289fc5ecf0726e2d47
suhaas-varma <65151864+suhaas-varma@users.noreply.github.com>
2020-06-25 19:23:18 +0530

    animations

--- ap.js
+++ ap.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
-  // Init ScrollMagic
   var controller = new ScrollMagic.Controller();
-  console.log("hello word");
-  // Scene intro - pin the intro section
 
   var tween = TweenMax.to("#animate1", 1, {
-    scale: 15,
+    scale: 129,
     ease: Linear.easeNone,
   });
 
@@ -23,22 +20,12 @@ $(document).ready(function () {
   });
   var scene2 = new ScrollMagic.Scene({
     triggerElement: "#uni",
-    triggerHook: 1,
+    triggerHook: 0,
     duration: "100%",
   })
     .setTween(tween2)
-    .addTo(controller);
-
-  var tween3 = TweenMax.to("#uni", 1, {
-    opacity: 0,
-    scale: 8,
-  });
-  var scene3 = new ScrollMagic.Scene({
-    triggerElement: "#plan",
-    triggerHook: 1,
-    duration: "100%",
-  })
-    .setTween(tween3)
+    .setPin("#uni")
+    .addIndicators()
     .addTo(controller);
 
   var tween4 = TweenMax.to("#plan", 1, {
@@ -46,32 +33,21 @@ $(document).ready(function () {
   });
   var scene4 = new ScrollMagic.Scene({
     triggerElement: "#plan",
-    triggerHook: 1,
+    triggerHook: 0,
     duration: "100%",
   })
     .setTween(tween4)
+    .setPin("#plan")
     .addTo(controller);
-
-  var tween5 = TweenMax.to("#plan", 1, {
-    opacity: 0,
-    scale: 8,
-  });
-  var scene5 = new ScrollMagic.Scene({
-    triggerElement: "#city",
-    triggerHook: 1,
-    duration: "100%",
-  })
-    .setTween(tween5)
-    .addTo(controller);
-
   var tween6 = TweenMax.to("#city", 1, {
     opacity: 1,
   });
   var scene6 = new ScrollMagic.Scene({
     triggerElement: "#city",
-    triggerHook: 1,
+    triggerHook: 0,
     duration: "100%",
   })
     .setTween(tween6)
+    .setPin("#city")
     .addTo(controller);
 });
